948011b1454d64f77e05500412e58d5d86f69030
Tina Rajaonarivelo <tina.rajao@hotmail.com>
2024-01-03 21:50:12 +0100

    feat: set comments route with join

---
+++ src/pages/api/posts/[postId]/comments.js
@@ -0,0 +1,75 @@
+import { validate } from "@/api/middlewares/validate"
+import mw from "@/api/mw"
+import auth from "@/pages/api/middlewares/auth"
+import { contentValidator, idValidator } from "@/utils/validators"
+
+// Revoir peut-être l'arborescence de l'API (ex: [postId]/comments => comments d'un post)
+
+const handle = mw({
+  POST: [
+    auth,
+    validate({
+      query: {
+        postId: idValidator,
+      },
+      body: {
+        content: contentValidator,
+      },
+    }),
+    async (ctx) => {
+      const {
+        input: {
+          body,
+          query: { postId },
+        },
+        models: { CommentModel },
+        res,
+        session: { id: userId },
+      } = ctx
+      const comment = await CommentModel.query()
+        .insertAndFetch({ postId, userId, ...body })
+        .withGraphFetched("[post,author]")
+        .modifyGraph("post", (builder) => {
+          builder.select("title", "content")
+        })
+        .modifyGraph("author", (builder) => {
+          builder.select("username")
+        })
+
+      res.send(comment)
+    },
+  ],
+  GET: [
+    auth,
+    validate({
+      query: {
+        postId: idValidator,
+      },
+    }),
+    async (ctx) => {
+      const {
+        input: {
+          query: { postId },
+        },
+        models: { CommentModel },
+        res,
+      } = ctx
+      const comments = await CommentModel.query()
+        .withGraphFetched("[post,author]")
+        .modifyGraph("post", (builder) => {
+          builder.select("title", "content")
+        })
+        .modifyGraph("author", (builder) => {
+          builder.select("username")
+        })
+        .where("postId", postId)
+        .orderBy("createdAt", "desc")
+
+      res.send(comments)
+    },
+  ],
+})
+
+export default handle
+
+// Ne pas oublier les kpis (comments count) pour le dashboard
