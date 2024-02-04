88530797016a2711e425eb1d32351cd41d42b95b
Tina Rajaonarivelo <tina.rajao@hotmail.com>
2024-02-04 17:35:59 +0100

    fix: change index page design

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,6 @@
 import Button from "@/web/components/ui/Buttons/Button"
+import Image from "next/image"
+import blogImg from "../../public/blog-img.jpg"
 import { useRouter } from "next/router"
 
 const IndexPage = () => {
@@ -8,26 +10,32 @@ const IndexPage = () => {
   }
 
   return (
-    <div className="h-full flex flex-col items-center justify-center">
-      <h1 className="p-4 text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-slate-400 to-slate-600">
+    <div className="h-full w-full flex flex-col items-center justify-center">
+      <h1 className="p-4 text-5xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-indigo-300 to-indigo-500">
         Welcome to the Blog App
       </h1>
-      <div className="p-4 flex flex-row items-center justify-center gap-4">
-        <Button
-          variant="secondary"
-          className={"rounded-md shadow-md hover:bg-slate-400"}
-          onClick={handleClick("/sign-in")}
-        >
-          Sign In
-        </Button>
-        <Button
-          variant="secondary"
-          href="/sign-up"
-          className={"rounded-md shadow-md hover:bg-slate-400"}
-          onClick={handleClick("/sign-up")}
-        >
-          Sign Up
-        </Button>
+      <Image src={blogImg} alt="blog" className="mt-4" />
+      <div className="flex flex-row gap-4 divide-x-2 divide-indigo-300 py-2">
+        <div className="p-4 flex flex-col items-center justify-center gap-2 w-1/2">
+          <p>Already have an account?</p>
+          <Button
+            className={"rounded-md shadow-md hover:bg-slate-400"}
+            onClick={handleClick("/sign-in")}
+          >
+            Sign In
+          </Button>
+        </div>
+        <div className="p-4 flex flex-col items-center justify-center gap-2 w-1/2">
+          <p className="text-center">
+            Don't have an account ? Want to discover more?
+          </p>
+          <Button
+            className={"rounded-md shadow-md hover:bg-slate-400"}
+            onClick={handleClick("/sign-up")}
+          >
+            Sign Up
+          </Button>
+        </div>
       </div>
     </div>
   )
